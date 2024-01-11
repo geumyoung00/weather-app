ab7d99b3beb7de99ca35095ecbbce1cd9484863f
geumyoung <1gyn093@gmail.com>
2024-01-12 00:43:13 +0900

    도시명을 검색해서 자동완성 리스트 생성, 리스트 항목 선택시 input의 value로 전달

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+import { City } from './types/index'
 
-function App() {
+const App = () => {
+  const [cities, setCities] = useState<City[]>([])
+  const [selectedCity, setSelectedCity] = useState<City | null>(null)
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    searchCity(value)
+    setInputValue(value)
+  }
+
+  const searchCity = async (value: string) => {
+    try {
+      const res = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&lang=en&appid=${process.env.REACT_APP_API_KEY}`
+      )
+      const { data } = res
+      setCities(data)
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const onClickItem = (item: City) => {
+    setSelectedCity(item)
+    setInputValue(`${item.name}, ${item.country}`)
+    setCities([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="flex justify-center items-center bg-gradient-to-b from-pink-400 via-purple-400 to-sky-400 h-[100vh] w-full">
+      <section className="w-full p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+        <h1 className="text-4xl font-thin">
+          <span className="font-black">Weather App</span>
+        </h1>
+        <p className="text-sm mt-2">어느 도시의 날씨가 궁금하신가요?</p>
+
+        <div className="relative flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={onChangeHandler}
+            className="px-2 py-1 rounded-l-md border-2 border-white"
+          />
+
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {cities.map((item, index) => (
+              <li key={`${item.lat} ${index}`}>
+                <button
+                  onClick={() => onClickItem(item)}
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                >
+                  {`${item.name}, ${item.country}`}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer">
+            search
+          </button>
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface City {
+  country: string
+  lat: string
+  lon: string
+  name: string
+}
