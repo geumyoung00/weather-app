db11db338570f4d9b0b55ab34b9920660cce9a54
geumyoung <1gyn093@gmail.com>
2024-01-13 01:15:12 +0900

    선택한 항목 제출하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import { City } from './types/index'
+import { City, CityData } from './types/index'
 
 const App = () => {
   const [cities, setCities] = useState<City[]>([])
-  const [selectedCity, setSelectedCity] = useState<City | null>(null)
+  const [selectedCity, setSelectedCity] = useState<null | City>(null)
   const [inputValue, setInputValue] = useState<string>('')
+  const [weatherData, setWeatherData] = useState<null | CityData>(null)
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
-    searchCity(value)
+    if (value) searchCity(value) //value가 없으면 실행하지 않음
     setInputValue(value)
   }
 
@@ -20,7 +21,6 @@ const App = () => {
       )
       const { data } = res
       setCities(data)
-      console.log(data)
     } catch (error) {
       console.error(error)
     }
@@ -32,40 +32,76 @@ const App = () => {
     setCities([])
   }
 
+  const getWeatherData = async () => {
+    try {
+      const res = await axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${
+          selectedCity!.lat
+        }&lon=${selectedCity!.lon}&units=metric&appid=${
+          process.env.REACT_APP_API_KEY
+        }`
+      )
+      const { data } = res
+      console.log('data__', data)
+      setWeatherData(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const onSubmit = () => {
+    getWeatherData()
+  }
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-b from-pink-400 via-purple-400 to-sky-400 h-[100vh] w-full">
-      <section className="w-full p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
-        <h1 className="text-4xl font-thin">
-          <span className="font-black">Weather App</span>
-        </h1>
-        <p className="text-sm mt-2">어느 도시의 날씨가 궁금하신가요?</p>
+      {weatherData ? (
+        <section className="w-full p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+          <h1 className="text-4xl font-thin">
+            <span className="font-black">{`${selectedCity!.name}, ${
+              selectedCity!.country
+            }의 날씨를 찾고 있어요 🤓`}</span>
+          </h1>
+          <br />
+          <p className="text-sm mt-2">잠시만 기다려주세요!</p>
+        </section>
+      ) : (
+        <section className="w-full p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+          <h1 className="text-4xl font-thin">
+            <span className="font-black">Weather App</span>
+          </h1>
+          <p className="text-sm mt-2">어느 도시의 날씨가 궁금하신가요?</p>
 
-        <div className="relative flex mt-10 md:mt-4">
-          <input
-            type="text"
-            value={inputValue}
-            onChange={onChangeHandler}
-            className="px-2 py-1 rounded-l-md border-2 border-white"
-          />
+          <div className="relative flex mt-10 md:mt-4">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={onChangeHandler}
+              className="px-2 py-1 rounded-l-md border-2 border-white"
+            />
 
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {cities.map((item, index) => (
-              <li key={`${item.lat} ${index}`}>
-                <button
-                  onClick={() => onClickItem(item)}
-                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
-                >
-                  {`${item.name}, ${item.country}`}
-                </button>
-              </li>
-            ))}
-          </ul>
+            <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+              {cities.map((item, index) => (
+                <li key={`${item.lat} ${index}`}>
+                  <button
+                    onClick={() => onClickItem(item)}
+                    className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                  >
+                    {`${item.name}, ${item.country}`}
+                  </button>
+                </li>
+              ))}
+            </ul>
 
-          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer">
-            search
-          </button>
-        </div>
-      </section>
+            <button
+              onClick={onSubmit}
+              className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer"
+            >
+              search
+            </button>
+          </div>
+        </section>
+      )}
     </main>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,35 @@ export interface City {
   lon: string
   name: string
 }
+
+export interface CityData {
+  country: string
+  name: string
+  sunrise: number
+  sunset: number
+  list: [
+    {
+      clouds: { all: number }
+      dt: number
+      main: {
+        feels_like: number
+        humidity: number
+        pressure: number
+        temp: number
+        temp_max: number
+        temp_min: number
+      }
+      pop: number
+      rain: { '3h': number }
+      visibility: number
+      weather: [
+        {
+          main: string
+          description: string
+          icon: string
+        }
+      ]
+      wind: { speed: number; deg: number; gust: number }
+    }
+  ]
+}
